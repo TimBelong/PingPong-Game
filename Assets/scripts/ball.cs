f9a6be2fa154f37f231963f1603ab30c45b4e304
Tim Belong <48470077+TimBelong@users.noreply.github.com>
2020-09-24 13:02:16 +0200

    Add files via upload

---
+++ Assets/scripts/ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ball : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        letItFly();
+    }
+
+    void letItFly()
+    {
+        speed = Random.Range(4, 8);
+        float randSc = Random.Range(0.25f, 1);
+        transform.localScale = new Vector2(randSc, randSc);
+        float x = Random.Range(-1, 1) == 0 ? -1 : 1;
+        float y = Random.Range(-1, 1) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(x * speed, y * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        reset();
+    }
+
+    public void reset ()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private int directionInt;
+    [SerializeField] private float speed = 4;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+        rb.velocity = new Vector2(speed * directionInt, rb.velocity.y);
+    }
+
+    public void Move(int InputAx)
+    {
+
+        directionInt = InputAx;
+
+    }
+}
